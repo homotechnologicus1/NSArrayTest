ccf63d2f2972a87fc3cdad217bfb76ebe18ffeb2
joe <homo.technologicus1@gmail.com>
2020-07-23 19:36:49 +0900

    Objective-C training

--- NSArrayTest/main.m
+++ NSArrayTest/main.m
@@ -10,8 +10,28 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSArray *month = [[NSArray alloc]initWithObjects:@"January",@"February",@"March",@"April",@"May",@"June",@"July",@"August",@"September", nil];
+        
+        /*
+        for (int i=0; i<[month count]; i++) {
+//            NSLog(@"month : %@", month[i]);
+            NSLog(@"month : %@", [month objectAtIndex:i]);
+        } */
+        
+        for (NSString *strTemp in month) {
+            NSLog(@"month : %@", strTemp);
+        }
+        
+        NSMutableArray *mmonth = [NSMutableArray arrayWithArray:month];
+        
+        [mmonth addObject:@"October"];
+        [mmonth addObject:@"November"];
+        [mmonth addObject:@"December"];
+        
+        for (NSString *strTemp in mmonth) {
+            NSLog(@"mmonth : %@", strTemp);
+        }
+        
     }
     return 0;
 }
